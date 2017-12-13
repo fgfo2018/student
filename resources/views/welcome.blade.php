4b82ce1853f0a5bf22f7c6935b7e1addabb4d932
fgfo2018 <fgfo2018@gmail.com>
2017-12-13 16:20:39 +0800

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    歡迎光臨MY PHP
                 </div>
 
                 <div class="links">
